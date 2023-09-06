33b4749fb5544387d7852b80d0d54f313f775724
r3vela710n <40471750+r3vela710n@users.noreply.github.com>
2023-09-05 20:21:33 -0400

    Updates some install commands

--- install.sh
+++ install.sh
@@ -21,17 +21,21 @@ echo "============================================"
 
 # install start
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+# sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(grep -Po 'UBUNTU_CODENAME=\K[^;]*' /etc/os-release) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 Installed=`sudo apt-cache policy docker-ce | sed -n '2p' | cut -c 14-`
 Candidate=`sudo apt-cache policy docker-ce | sed -n '3p' | cut -c 14-`
 
 if [[ "$Installed" != "$Candidate" ]]; then
-	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 elif [[ "$Installed" == "$Candidate" ]]; then
 	echo ""
 	echo 'docker currently version already installed.'
